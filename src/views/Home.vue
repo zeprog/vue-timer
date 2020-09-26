b515379c526f72ced7bec5059ee0fd0bb78f15f8
zeprog <dmitrium02@mail.ru>
2020-09-27 02:27:44 +0300

    Чистый проект с фоном

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Timer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Timer from '@/components/Timer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Timer
   }
 }
 </script>
+
+<style lang='scss'>
+.home {
+    background: #353638;
+    height: 100vh;
+}
+</style>
